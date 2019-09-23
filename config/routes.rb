47c6140114441b30d114610ceb5212f8dca937af
Kevin Karma <kevinkarma55@gmail.com>
2019-09-23 02:00:59 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get '/coupons', to: 'coupons#index', as: 'coupons'
   get '/coupons/new', to: 'coupons#new', as: 'new_coupon'
   get '/coupons/:id', to: 'coupons#show', as: 'coupon'
-  post '/coupons', to: 'coupons#create', as 'create_coupon'
+  post '/coupons', to: 'coupons#create', as: 'create_coupon'
 end
