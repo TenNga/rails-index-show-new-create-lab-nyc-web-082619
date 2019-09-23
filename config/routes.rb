f2c396e7ffb4f10cbe573785e3211b577cfcf488
Kevin Karma <kevinkarma55@gmail.com>
2019-09-23 01:47:59 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'coupons/index'
-
-  get 'coupons/show'
-
-  get 'coupons/new'
-
-  get 'coupons/create'
-
   get '/coupons', to: 'coupons#index', as: 'coupons'
+  get '/coupons/new', to: 'coupons#new', as: 'new_coupon'
 end
